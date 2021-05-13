463d095ecae9a5ec46cf2c0b714e3dd03b1313cd
Iven S <47639297+LvckyAPI@users.noreply.github.com>
2021-05-13 22:13:35 +0200

    [+] MySQL [+] SchotterManager [+] SystemManager

--- src/main/java/net/sernoxcraft/schottersystem/mysql/MySQL.java
+++ src/main/java/net/sernoxcraft/schottersystem/mysql/MySQL.java
@@ -1,8 +1,65 @@
 package net.sernoxcraft.schottersystem.mysql;
 
+import net.sernoxcraft.schottersystem.systemmanager.SystemManager;
+import org.bukkit.Bukkit;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * ©2016-2021 LvckyWorld - By StossenHDYT all Rights reserved
  * Licensed to Iven Schlenther & Lukas Oetken
  **/
 public class MySQL {
+
+
+
+    public static Connection con;
+
+    public static String sqlPrefix = "§c[MySQL] §e";
+
+    /**
+     * Stellt die Verbindung mit der Datenbank her
+     *
+     * @throws SQLException Catchen der Exception vom Datenbank Verbindungsaufbau
+     * @author LvckyAPI
+     * @since 10/05/2021
+     */
+    public static void connect() throws SQLException {
+        if (!isConnected()) {
+            con = DriverManager.getConnection("jdbc:mysql://" + SystemManager.getHOST() + ":3306/" +
+                    SystemManager.getDATABASE() + "?autoReconnect=true", SystemManager.getUSER(), SystemManager.getPASSWORD());
+            Bukkit.getConsoleSender().sendMessage(sqlPrefix + "Verbindung erfolgreich hergestellt.");
+        }
+    }
+
+    /**
+     * Schließt die Datenbank verbindung wenn eine Verbindung besteht
+     *
+     * @throws SQLException
+     * @author LvckyAPI
+     * @since 10/05/2021
+     */
+    public static void disconnect() throws SQLException {
+        if (isConnected()) {
+            con.close();
+            Bukkit.getConsoleSender().sendMessage(sqlPrefix + "Verbindung wurde erfolgreich geschlossen");
+        }
+    }
+
+    /**
+     * Überprüft ob eine Aktive Verbindung besteht
+     * @since 10/05/2021
+     * @return verbunden/nicht verbunden
+     */
+    public static boolean isConnected() {
+        return (con != null);
+    }
+
+    public static Connection getConnection() {
+        return con;
+    }
+
+
 }
--- src/main/java/net/sernoxcraft/schottersystem/systemmanager/SystemManager.java
+++ src/main/java/net/sernoxcraft/schottersystem/systemmanager/SystemManager.java
@@ -1,8 +1,144 @@
 package net.sernoxcraft.schottersystem.systemmanager;
 
+import net.sernoxcraft.schottersystem.main.Main;
+import net.sernoxcraft.schottersystem.mysql.MySQL;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static net.sernoxcraft.schottersystem.mysql.MySQL.sqlPrefix;
+
 /**
  * ©2016-2021 LvckyWorld - By StossenHDYT all Rights reserved
  * Licensed to Iven Schlenther & Lukas Oetken
  **/
 public class SystemManager {
+
+    /* Erstellung von statischen Strings */
+    /* Für Database login */
+    private static String HOST;
+    private static String PORT;
+    private static String DATABASE;
+    private static String USER;
+    private static String PASSWORD;
+
+
+    FileConfiguration cfg = Main.getPlugin().getConfig();
+    String pref = cfg.getString("Config.prefix").replaceAll("&","§") + "§r ";
+
+    private static String prefix;
+
+    private static final String debugPrefix =  prefix + "§b[Debug] §4";
+
+
+
+
+    public static void startUp() {
+        prefix = "";
+        Bukkit.getConsoleSender().sendMessage(debugPrefix + "startUp()");
+        loadConfig();
+        readMySQL();
+
+        FileConfiguration cfg = getConfiguration();
+        Boolean MySQLAllowed = cfg.getBoolean("mysql.use");
+        if (MySQLAllowed) {
+            try {
+                Bukkit.getConsoleSender().sendMessage(debugPrefix + "setup() in startUp()");
+                setup();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void setup() throws SQLException {
+        Bukkit.getConsoleSender().sendMessage(debugPrefix + "setup()");
+        FileConfiguration cfg = getConfiguration();
+        MySQL.connect();
+        PreparedStatement ps = MySQL.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS SchotterSystem (UUID VARCHAR(100), Spielername VARCHAR(100),Balance BIGINT)");
+        ps.executeUpdate();
+        Bukkit.getConsoleSender().sendMessage(sqlPrefix + "§aSchotterSystem registriert");
+    }
+
+
+    private static File getMySQL() {return new File("plugins/SchotterSystem/MySQL.yml");}
+    private static FileConfiguration getConfiguration() { return YamlConfiguration.loadConfiguration(getMySQL()); }
+
+    private static void loadConfig() {
+        if (!getMySQL().exists()) {
+            Bukkit.getConsoleSender().sendMessage(debugPrefix + "loadConfig()");
+            FileConfiguration cfg = getConfiguration();
+            cfg.set("mysql.use", false);
+            cfg.set("mysql.host", "< Deinen MySQL Hoster eintragen >");
+            cfg.set("mysql.port", "< Deinen MySQL Port eintragen >");
+            cfg.set("mysql.user", "< Deinen MySQL User eintragen >");
+            cfg.set("mysql.datenbase", "< Deine MySQL Datenbase eintragen >");
+            cfg.set("mysql.passwort", "< Dein MySQL Passwort eintragen >");
+            try {
+                cfg.save(getMySQL());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private static void readMySQL() {
+        Bukkit.getConsoleSender().sendMessage(debugPrefix + "readMySQL");
+        FileConfiguration cfg = getConfiguration();
+        setHOST(cfg.getString("mysql.host"));
+        setPORT(cfg.getString("mysql.user"));
+        setUSER(cfg.getString("mysql.user"));
+        setDATABASE(cfg.getString("mysql.datenbase"));
+        setPASSWORD(cfg.getString("mysql.passwort"));
+    }
+
+
+
+    /* Erstellung der Getter */
+    public static String getHOST() {
+        return HOST;
+    }
+
+    public static void setHOST(String hOST) {
+        HOST = hOST;
+    }
+
+    public static String getPORT() {
+        return PORT;
+    }
+    public static void setPORT(String pORT) {
+        PORT = pORT;
+    }
+
+    public static String getDATABASE() {
+        return DATABASE;
+    }
+
+    public static void setDATABASE(String dATABASE) {
+        DATABASE = dATABASE;
+    }
+
+    public static String getUSER() {
+        return USER;
+    }
+
+    public static void setUSER(String uSER) {
+        USER = uSER;
+    }
+
+    public static String getPASSWORD() {
+        return PASSWORD;
+    }
+
+    public static void setPASSWORD(String pASSWORD) {
+        PASSWORD = pASSWORD;
+    }
+
 }
