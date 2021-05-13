3f86cc4f917dcf875f7f4056e31d2bce765b2252
Markensau <oetkenlukas@gmail.com>
2021-05-13 21:53:03 +0200

    .

---
+++ src/main/java/net/sernoxcraft/schottersystem/mysql/MySQL.java
@@ -0,0 +1,8 @@
+package net.sernoxcraft.schottersystem.mysql;
+
+/**
+ * ©2016-2021 LvckyWorld - By StossenHDYT all Rights reserved
+ * Licensed to Iven Schlenther & Lukas Oetken
+ **/
+public class MySQL {
+}
---
+++ src/main/java/net/sernoxcraft/schottersystem/systemmanager/SystemManager.java
@@ -0,0 +1,8 @@
+package net.sernoxcraft.schottersystem.systemmanager;
+
+/**
+ * ©2016-2021 LvckyWorld - By StossenHDYT all Rights reserved
+ * Licensed to Iven Schlenther & Lukas Oetken
+ **/
+public class SystemManager {
+}
