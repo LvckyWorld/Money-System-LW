4fb246bce6b6a97a627388d67c7cad2642a21566
Markensau <oetkenlukas@gmail.com>
2021-05-13 22:06:53 +0200

    [+] PayCommand without Database

--- src/main/java/net/sernoxcraft/schottersystem/commands/PayCommand.java
+++ src/main/java/net/sernoxcraft/schottersystem/commands/PayCommand.java
@@ -1,5 +1,7 @@
 package net.sernoxcraft.schottersystem.commands;
 
+import net.sernoxcraft.schottersystem.main.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,8 +14,44 @@ import org.bukkit.entity.Player;
 public class PayCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        // /pay <user> <geld>
         if (sender instanceof Player){
+            Player p = (Player) sender;
+            if (args.length == 1){
+                Player target = Bukkit.getPlayer(args[0]);
+                    //Spieler der das Geld bekommen soll.
+                if (target == null) {
+                    p.sendMessage(Main.prefix + "§cDer Spieler ist nicht online!");
+                    return false;
+                }
+                int sum = 1;
+                try{
+                    sum = Integer.parseInt(args[1]);
+                }catch (Exception e){
+                    p.sendMessage(Main.prefix + "§cBitte gebe eine gültige Summe an!");
+                }
 
+                //MySQL part
+                //Sender
+                int balancePlayerSender = 2;
+                //Target
+                int balancePlayerTarget;
+
+                if (balancePlayerSender > sum){
+
+
+
+
+                } else {
+                    p.sendMessage(Main.prefix + "§cSo viel Geld hast du nicht!");
+                    return false;
+                }
+
+
+
+            } else {
+                p.sendMessage(Main.prefix + "§cFalsche Usage. Benutze bitte§7: §b/pay <user> <summe>");
+            }
         }
         return false;
     }
