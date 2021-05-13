ec0ecf59d18ab3daefe5829a9923a3a806bc59a0
Iven S <47639297+LvckyAPI@users.noreply.github.com>
2021-05-13 22:49:55 +0200

    [+] BalanceCommand

--- src/main/java/net/sernoxcraft/schottersystem/commands/BalanceCommand.java
+++ src/main/java/net/sernoxcraft/schottersystem/commands/BalanceCommand.java
@@ -1,5 +1,8 @@
 package net.sernoxcraft.schottersystem.commands;
 
+import net.sernoxcraft.schottersystem.main.Main;
+import net.sernoxcraft.schottersystem.utils.SchotterManager;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,6 +16,29 @@ public class BalanceCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player){
+            Player p = (Player) sender;
+            if (args.length == 0) {
+                p.sendMessage(Main.prefix + "§3Du hast §b" + SchotterManager.getBalance(p) + "§3 Schotter");
+            } else if (args.length == 1){
+                if (p.hasPermission("ss.balance.see.others")) {
+                    Player t = Bukkit.getPlayer(args[0]);
+                    if (t != null) {
+                        p.sendMessage(Main.prefix + "§3Der Spieler §e" + t.getName() + "§3 hat §b" + SchotterManager.getBalance(t) + "§3 Schotter");
+                    } else {
+                        p.sendMessage("§cDer spieler ist nicht online");
+                    }
+                } else {
+                    p.sendMessage(Main.prefix + "§cDu hast keine rechte, §e" + label + "§c zu benutzen");
+                }
+            } else {
+                if (p.hasPermission("ss.balance.see.others")) {
+                    p.sendMessage(Main.prefix + "§3Mache §e/balance <Spieler>");
+                } else {
+                    p.sendMessage("§3Mache §e/balance ");
+                }
+
+            }
+
 
         }
         return false;
