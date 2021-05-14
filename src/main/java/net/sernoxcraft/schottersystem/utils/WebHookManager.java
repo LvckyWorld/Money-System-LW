ed5587d947ba196bbdda09cf8a5c281123d4291e
Markensau <oetkenlukas@gmail.com>
2021-05-14 13:21:34 +0200

    [+] WebHook Embed

--- src/main/java/net/sernoxcraft/schottersystem/utils/WebHookManager.java
+++ src/main/java/net/sernoxcraft/schottersystem/utils/WebHookManager.java
@@ -1,8 +1,18 @@
 package net.sernoxcraft.schottersystem.utils;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import javax.net.ssl.HttpsURLConnection;
+import java.awt.*;
 import java.io.OutputStream;
 import java.net.URL;
+import java.sql.Time;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 
 /**
  * ©2016-2021 LvckyWorld - By StossenHDYT all Rights reserved
@@ -11,8 +21,24 @@ import java.net.URL;
 public class WebHookManager {
 
 
-    public static void onSendDiscordMessage(String content, String username, String WebHookURL) throws Exception {
-        String json = "{\"content\": \"" + content + "\", \"username\": \"" + username + "\"}";
+    public static void onSendDiscordMessage(String title, String content, String username, String WebHookURL) throws Exception {
+
+        JSONObject embed = new JSONObject();
+        embed.put("title", title);
+        embed.put("description", content);
+        embed.put("color", 16745963);
+        embed.put("timestamp", LocalDateTime.now().toString());
+
+        JSONArray embeds = new JSONArray();
+        embeds.add(embed);
+
+
+        JSONObject json = new JSONObject();
+        json.put("username", username);
+        json.put("embeds", embeds);
+
+
+        //String json = "{\"content\": \"" + content + "\", \"username\": \"" + username + "\"}";
 
         URL url = new URL(WebHookURL);
         HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
@@ -28,6 +54,8 @@ public class WebHookManager {
 
         connection.getInputStream().close();
         connection.disconnect();
+
+
     }
 
 }
