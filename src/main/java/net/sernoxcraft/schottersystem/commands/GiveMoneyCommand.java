3bccb735ecccb647ba6bb4f844c4438910fead48
Iven S <47639297+LvckyAPI@users.noreply.github.com>
2021-05-14 13:33:51 +0200

    [+] SetMoneyCommand [+] GiveMoneyCommand

---
+++ src/main/java/net/sernoxcraft/schottersystem/commands/GiveMoneyCommand.java
@@ -0,0 +1,9 @@
+package net.sernoxcraft.schottersystem.commands;
+/*
+ * ©2016-2021 LvckyWorld - By StossenHDYT all Rights reserved
+ * Licensed to Iven Schlenther & Lukas Oetken
+ */
+
+
+public class GiveMoneyCommand {
+}
